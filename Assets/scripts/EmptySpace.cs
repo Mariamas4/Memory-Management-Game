e159c32d13d08b42704ad312fb6afbf85230b022
Mariam <mariam.asfour@gmail.com>
2015-12-23 00:14:56 +0200

    first commit

---
+++ Assets/scripts/EmptySpace.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptySpace : MonoBehaviour {
+
+	public Transform Block, positionOfNewProcess;
+	public int valueSpace;
+	public TextMesh text;
+
+	void Awake()
+	{
+		valueSpace = (int)(Block.localScale.y * 1000);
+
+	}
+
+	void Start () {
+	
+		text.text = valueSpace.ToString();
+
+	}
+
+	public void UpdateText()
+	{
+		valueSpace = (int)(Block.localScale.y * 1000);
+		text.text = valueSpace.ToString();
+
+	}
+
+
+	void Update () {
+	
+	}
+
+
+	void OnMouseDown ()
+	{
+		Debug.Log ("Screen: world: ");
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+
+	}
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject spawn, rightFreeSpace;
+	public GameObject []EmptySpaces;
+	public GameObject[]newProcesses;
+//	public List<GameObject> EmptySpaces;
+	public GameObject [] arrangedEmptySpaces;
+	enum GameType {MEMORYALLOCATION, PAGING};
+	public enum AllocationType {FIRSTFIT, NEXTFIT, BESTFIT, WORSEFIT}
+	public enum State {GENERATE, WAIT, ASSES, FINALSCORE};
+	GameType gameType;
+	public AllocationType allocType;
+	public State currentSate;
+	bool gameOver, lastProcess;
+
+	public int numberOfNewProcesses, currentProcessNum;
+	public GameObject currentProcessToAllocate;
+
+	public float Score = 100;
+	RaycastHit hit;
+
+	void Start () {
+		arrangedEmptySpaces = new GameObject[EmptySpaces.Length];
+		for(int i =0; i < EmptySpaces.Length; ++i)
+			arrangedEmptySpaces[i] = EmptySpaces[i];
+		numberOfNewProcesses = newProcesses.Length;
+
+
+	}
+
+
+	void Update () {
+	
+		if (currentSate == State.WAIT) {
+			if (Input.GetMouseButtonDown (0)) {
+				if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100.0f)) {
+					Debug.Log (hit.collider.name);
+
+					if(hit.collider.tag == "FreeSpace"){
+
+						if(hit.collider.gameObject == rightFreeSpace)
+						{
+							Debug.Log("Same" + rightFreeSpace.transform.localScale.y);
+							currentProcessToAllocate.transform.position = rightFreeSpace.GetComponent<EmptySpace> ().positionOfNewProcess.position;
+							Vector3 newVector =  rightFreeSpace.transform.localScale;
+							float newFreeSpaceamount = rightFreeSpace.transform.localScale.y - currentProcessToAllocate .transform.localScale.y;
+							newVector.y = newFreeSpaceamount; 
+							rightFreeSpace.transform.localScale = newVector;
+							rightFreeSpace.GetComponent<EmptySpace>().UpdateText();
+
+
+							if (lastProcess)
+								currentSate = State.FINALSCORE;
+							else 
+								currentSate = State.GENERATE;
+
+						}else if(hit.collider.gameObject != rightFreeSpace){
+							// Wrong get less score
+						}
+					}
+				}
+			}
+		}
+
+		switch (currentSate) {		
+		case State.GENERATE:
+			//Generating
+			currentProcessToAllocate = GameObject.Instantiate (newProcesses [currentProcessNum],
+						                       spawn.transform.position,
+			                                   newProcesses [currentProcessNum].transform.rotation) as GameObject;
+
+
+			currentProcessNum++;
+			if (currentProcessNum == newProcesses.Length)
+				lastProcess = true;
+			Debug.Log(currentProcessToAllocate);
+			Debug.Log(currentProcessToAllocate.GetComponent<EmptySpace> ().valueSpace);
+
+			FirstFitAllocation ();
+			
+			currentSate = State.WAIT;
+			break;
+		case State.WAIT:
+			
+			break;
+		case State.ASSES:
+		
+
+			break;
+		case State.FINALSCORE:
+		
+			break;
+		}
+	}
+
+	public void changeAllocType(int type)
+	{
+//		Debug.Log ("Ha: "+type);
+		allocType = (AllocationType)type;
+		if(allocType== AllocationType.BESTFIT || allocType == AllocationType.WORSEFIT)
+			ArrangeEmptySpaces ();
+		else if(allocType == AllocationType.FIRSTFIT)
+			for(int i =0; i < EmptySpaces.Length; ++i)
+				arrangedEmptySpaces[i] = EmptySpaces[i];
+	}
+
+	public void ArrangeEmptySpaces()
+	{
+		GameObject temp;
+		if (allocType == AllocationType.BESTFIT) {
+			Debug.Log ("Hey U!");
+
+			for (int i = 0; i < arrangedEmptySpaces.Length; i++) {
+				for (int sort = 0; sort < arrangedEmptySpaces.Length - 1; sort++) {
+					if (arrangedEmptySpaces[sort].GetComponent<EmptySpace>().valueSpace > arrangedEmptySpaces[sort + 1].GetComponent<EmptySpace>().valueSpace) {
+						temp = arrangedEmptySpaces[sort + 1];
+						arrangedEmptySpaces[sort + 1] = arrangedEmptySpaces[sort];
+						arrangedEmptySpaces[sort] = temp;
+					}
+				}
+			}
+
+		} else if (allocType == AllocationType.WORSEFIT) {
+			for (int i = 0; i < arrangedEmptySpaces.Length; i++) {
+				for (int sort = 0; sort < arrangedEmptySpaces.Length - 1; sort++) {
+					if (arrangedEmptySpaces[sort].GetComponent<EmptySpace>().valueSpace < arrangedEmptySpaces[sort + 1].GetComponent<EmptySpace>().valueSpace) {
+						temp = arrangedEmptySpaces[sort + 1];
+						arrangedEmptySpaces[sort + 1] = arrangedEmptySpaces[sort];
+						arrangedEmptySpaces[sort] = temp;
+					}
+				}
+			}
+		}
+	
+		FirstFitAllocation ();
+	}
+	void FirstFitAllocation ()
+	{
+		for (int i = 0; i<arrangedEmptySpaces.Length; ++i) {
+			if (currentProcessToAllocate.GetComponent<EmptySpace> ().valueSpace < arrangedEmptySpaces [i].GetComponent<EmptySpace> ().valueSpace) 
+			{
+				//	Handle moving
+//				Debug.Log(currentProcessToAllocate.GetComponent<EmptySpace> ().valueSpace);
+//				Debug.Log(arrangedEmptySpaces [i].GetComponent<EmptySpace> ().valueSpace);
+				rightFreeSpace = arrangedEmptySpaces [i];
+				Debug.Log("Choosen space: "+i);
+				break;
+			}
+			Debug.Log ("No enough Space!!");
+
+		}
+
+	}
+
+}
